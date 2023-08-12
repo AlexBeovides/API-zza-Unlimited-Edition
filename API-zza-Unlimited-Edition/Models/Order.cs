543ba3fdc47823f3766a7d5e5b28e121ef8d0efb
AlexBeovides <abasbeovides@gmail.com>
2023-08-12 10:09:09 +0200

    added models

---
+++ API-zza-Unlimited-Edition/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Order
+{
+    public int OrderNumber { get; set; }
+    public DateTime OrderDate { get; set; }
+    public List<Pizza> Pizzas { get; set; }
+    public decimal TotalAmount { get; set; }
+    
+}
---
+++ API-zza-Unlimited-Edition/Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Pizza
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public List<Ingredient> Toppings { get; set; }
+    
+}
